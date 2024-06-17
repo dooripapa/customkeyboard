fab3258ba2f46c72af42a80511c7edb593a7c6f2
dooripapa <b532882@hotmail.com>
2024-06-17 23:07:22 +0900

    update

--- keyboards/crkbd/rev1/keymaps/b532882/keymap.c
+++ keyboards/crkbd/rev1/keymaps/b532882/keymap.c
@@ -48,9 +48,9 @@ tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_split_3x6_3(
-    KC_TAB   , KC_Q      , KC_W   , KC_E    , KC_R       , KC_T         , KC_Y         , KC_U    , KC_I     , KC_O     , KC_P    , KC_BSPC      ,
-    KC_LCTL  , LT(6,KC_A), KC_S   , KC_D    , LT(5,KC_F) , KC_G         , KC_H         , KC_J    , KC_K     , KC_L     , KC_SCLN , LT(0,KC_ENT) ,
-    KC_LSFT  , KC_Z      , KC_X   , KC_C    , KC_V       , KC_B         , KC_N         , KC_M    , KC_COMM  , KC_DOT   , KC_SLSH , KC_RSFT      ,
+    KC_TAB   , KC_Q      , KC_W   , KC_E    , KC_R       , KC_T         , KC_Y         , KC_U    , KC_I     , KC_O     , KC_P    , KC_BSPC ,
+    KC_LCTL  , LT(6,KC_A), KC_S   , KC_D    , LT(5,KC_F) , KC_G         , KC_H         , KC_J    , KC_K     , KC_L     , KC_SCLN , KC_ENT  ,
+    KC_LSFT  , KC_Z      , KC_X   , KC_C    , KC_V       , KC_B         , KC_N         , KC_M    , KC_COMM  , KC_DOT   , KC_SLSH , KC_RSFT ,
                                     KC_LGUI , KC_LALT    , LT(1,KC_SPC) , LT(2,KC_SPC) , KC_RALT , MO(3)
 ),
 
@@ -83,10 +83,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [5] = LAYOUT_split_3x6_3(
-    _______  , KC_NO      , KC_NO      , KC_NO      , KC_NO       , KC_NO    , KC_NO       , KC_7      , KC_8      , KC_9   , KC_0       , _______ ,
-    _______  , KC_NO      , KC_NO      , KC_NO      , KC_NO       , KC_NO    , KC_DOT      , KC_4      , KC_5      , KC_6   , KC_NO      , _______ ,
-    _______  , KC_NO      , KC_NO      , KC_NO      , KC_NO       , KC_NO    , KC_COMM     , KC_1      , KC_2      , KC_3   , KC_SLSH    , _______ ,
-                                           _______  , _______     , KC_NO    , KC_NO       , _______   , _______
+    _______  , KC_NO      , KC_NO      , KC_NO      , KC_NO     , KC_NO        , KC_NO        , KC_7      , KC_8      , KC_9   , KC_0       , _______ ,
+    _______  , KC_NO      , KC_NO      , KC_NO      , KC_NO     , KC_NO        , KC_DOT       , KC_4      , KC_5      , KC_6   , KC_NO      , C(S(KC_ENT)) ,
+    _______  , KC_NO      , KC_NO      , KC_NO      , KC_NO     , KC_NO        , KC_COMM      , KC_1      , KC_2      , KC_3   , KC_SLSH    , _______ ,
+                                           _______  , _______   , LT(1,KC_SPC) , LT(2,KC_SPC) , _______   , _______
 ),
 
 [6] = LAYOUT_split_3x6_3(
